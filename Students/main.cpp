969501f25b808eafda9c7f79d2c46d54e3f78ce3
goranknezevic <knezevichgoran@gmail.com>
2017-05-16 14:21:50 +0200

    Students Utils refactored

--- Students/main.cpp
+++ Students/main.cpp
@@ -7,9 +7,6 @@
 using namespace std;
 using namespace studentUtils;
 
-int countRecords(ifstream& inFile);
-
-void storeRecordsInArray(ifstream& inFile, string names[], int result[]);
 void checkStudentResult(string name, int result,string grade);
 
 Student bestStudent;
@@ -63,31 +60,4 @@ void checkStudentResult(string name, int result,string grade) {
     if (worstStudent.getResult() == -1 || worstStudent.getResult() > result) {
         putNewResult(worstStudent, name, result, grade);
     }
-}
-
-void storeRecordsInArray(ifstream& inFile, string names[], int result[]) {
-    char data[100];
-    int row = 0;    
-    
-    while (!inFile.eof()) {
-        inFile.getline(data, 100);
-        string line  = data;
-        int indexOfSpace = line.find(" ");
-        names[row] = line.substr(0,indexOfSpace);
-        string singleResult = line.substr(indexOfSpace, line.length());
-        stringstream(singleResult) >> result[row];
-        row++;
-    }
-}
-
-int countRecords(ifstream& inFile) {
-    char data[100];
-    int totalRecords = 0;
-    
-    while (!inFile.eof()) {
-        inFile.getline(data, 100);
-        totalRecords++;
-    }
-
-    return totalRecords;
 }
--- Students/studentUtils.h
+++ Students/studentUtils.h
@@ -3,6 +3,7 @@
 using namespace std;
 
 namespace studentUtils{
+
     void putNewResult(Student& student, string name, int result, string grade) {
         student.setName(name);
         student.setResult(result);
@@ -17,5 +18,32 @@ namespace studentUtils{
             return "DI";
         }
         return "CR";
+    }
+
+    void storeRecordsInArray(ifstream& inFile, string names[], int result[]) {
+        char data[100];
+        int row = 0;    
+        
+        while (!inFile.eof()) {
+            inFile.getline(data, 100);
+            string line  = data;
+            int indexOfSpace = line.find(" ");
+            names[row] = line.substr(0,indexOfSpace);
+            string singleResult = line.substr(indexOfSpace, line.length());
+            stringstream(singleResult) >> result[row];
+            row++;
+        }
+    }
+
+    int countRecords(ifstream& inFile) {
+        char data[100];
+        int totalRecords = 0;
+        
+        while (!inFile.eof()) {
+            inFile.getline(data, 100);
+            totalRecords++;
+        }
+
+        return totalRecords;
     }
 }
